6b3dbbad9b991f991c4c63f1c8c9bf9c3545c432
Damola Oladeji <darmhoo@hotmail.com>
2024-04-11 17:54:07 +0000

    removed it

--- database/migrations/2024_03_26_144252_create_profiles_table.php
+++ database/migrations/2024_03_26_144252_create_profiles_table.php
@@ -13,8 +13,8 @@ return new class extends Migration {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('last_name')->nullable();
-            $table->string('first_name')->nullable();
+            $table->string('last_name');
+            $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('gender')->nullable();
             $table->string('phone')->unique()->nullable();
