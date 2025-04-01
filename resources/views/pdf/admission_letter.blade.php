85c377f4b13267c771daa5d7002925e617d5beb1
Omodamola Oladeji <darmhoo@hotmail.com>
2025-04-01 21:10:22 +0000

    done

--- resources/views/pdf/admission_letter.blade.php
+++ resources/views/pdf/admission_letter.blade.php
@@ -44,41 +44,57 @@
                     <div>
                         We are so excited to welcome you to International School of Ministry 2025!
                     </div>
+                    <br>
 
 
                     <div>It is going to be an intensive 3-Months of solid training where you will receive insightful and
                         transformative teachings on the foundational rudiments of ministry. The course will equip,
                         empower and sharpen you to deliver God’s counsel to your generation as you will come out
                         refined.</div>
+                    <br>
                     <div>The course will include thorough assessments, projects, practicals and ministry work. It will
                         indeed be a training ground and a platform to grow into all God has called you to be.</div>
+                    <br>
                     <div>You will be tutored by a faculty of seasoned facilitators as led by the Principal, Apostle Femi
                         Lazarus. Be rest assured you are in great hands.</div>
-                    <div>Your Registration number is <b>{{ $matric_no }}</b></div>
+                    <br>
+                    <div><i>Your Registration number is <b>{{ $matric_no }}</b></i></div>
+                    <br>
+                    <div><i>Your Registration number is <b>{{ $group_no }}</b></i></div>
+                    <br>
+
                     {{-- <div>Your Group number is <b>{{ $group_no }}</b></div> --}}
                     <div>Do well to keep this Registration no. safe as this will be used for your online assessments and
                         tests during the session of the school. Your group number determines your project topic and
                         field
                         work assignments.</div>
+                    <br>
                     <div>ISM 2024 will run as both online and onsite.</div>
+                    <br>
                     <div>On-site classes:
+                        <br>
                         <ul>
-                            <li>Ibadan Campus: Light Nation Auditorium, Oshuntokun Avenue, Bodija, Ibadan</li>
+                            <li>Ibadan Campus: Light Nation Auditorium, Oshuntokun Avenue, Bodija, Ibadan</li><br>
                             <li>Abuja Campus: Light Place Event Center, Beside Nigerian Institute of Transport
                                 Technology,
                                 Off Asuquo Okon Street,
                                 Utako, Federal Capacity Territory.</li>
                         </ul>
                     </div>
+                    <br>
                     <div>Online classes will be streamed online and you can attend at the comfort of your home or office
                         or
                         anywhere.</div>
+                    <br>
                     <div>The session will run from April 5th - June 13th 2025. Lectures commences on Saturday, 5th April
                         2025 with an introductory/Orientation class. The course schedule and curriculum for the entire
                         two months of the school will be sent to you.</div>
+                    <br>
                     <div>We welcome you once again to this year’s edition of International School of Ministry and we
                         wish you a life transforming experience!</div>
+                    <br>
                     <div>For further enquires and questions: +2349030959735, +2349034646810</div>
+                    <br>
                     <div>Regards.</div>
                 </div>
 
