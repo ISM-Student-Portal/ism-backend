92f15c05053e111a8642e90e7356fae2a852997a
Omodamola Oladeji <darmhoo@hotmail.com>
2025-04-02 15:38:51 +0000

    try chunking

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -363,28 +363,31 @@ class UserController extends Controller
 
     public function updateUserMails(Request $request)
     {
-        $students = Student::query()->where('matric_no', '!=', null)->get();
-        // dd($students);
         $done = [];
 
-        foreach ($students as $student) {
-            try {
-                $password = Str::password(8, true, true, false, false);
-                $student->update([
-                    'password' => bcrypt($password)
-                ]);
-                // dd('got here');
-                Mail::to($student)->later(now()->addSeconds(5), new StudentOnboarding($student, $password));
+        $students = Student::query()->where('matric_no', '!=', null)->chunk(50, function ($stu) {
+            foreach ($stu as $student) {
+                try {
+                    $password = Str::password(8, true, true, false, false);
+                    $student->update([
+                        'password' => bcrypt($password)
+                    ]);
+                    // dd('got here');
+                    Mail::to($student)->later(now()->addSeconds(5), new StudentOnboarding($student, $password));
 
-                array_push($done, $student);
-                //code...
-            } catch (Exception $th) {
-                //throw $th;
-                $errors = [];
-                array_push($errors, $th);
+                    // array_push($done, $student);
+                    //code...
+                } catch (Exception $th) {
+                    //throw $th;
+                    $errors = [];
+                    array_push($errors, $th);
+                }
+
             }
+        });
+        // dd($students);
 
-        }
+
 
 
         return response()->json([
